2ce380b885c6b321e3d5886d4e8a0a23a1db07d7
Robbie Luijben <diggingforfire@gmail.com>
2017-05-13 14:46:02 +0200

    Missing semicolon, thanks linter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LobbyComponent } from './lobby/lobby.component'
+import { LobbyComponent } from './lobby/lobby.component';
 
 @Component({
 	selector: 'app',
